8a8815193e238bb65da249b2f537b17dbab1f574
Godones <1925466036@qq.com>
2022-11-18 21:28:15 +0800

    feat: update memory module

--- dependence/double-link-list/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = 5;
-    let p1 = &x as *const _ as *const i32;
-    let p2 = &&x as *const _ as *const i32;
-    println!("p1 = {:p}", p1);
-    println!("p2 = {:p}", p2);
-}
--- kernel/src/config.rs
+++ kernel/src/config.rs
@@ -1,9 +1,6 @@
 /// 内核堆的大小
-pub const KERNEL_HEAP_SIZE: usize = 0xc0_0000;
+// pub const KERNEL_HEAP_SIZE: usize = 0xc0_0000;
 
 pub const RISCV_UART_ADDR: usize = 0x10000000;
 pub const MEMORY_END: usize = 0x1_0000_0000; //2GB
 pub const FRAME_SIZE: usize = 0x1000; //4KB
-
-/// slab
-pub const CACHE_NAME_MAX: usize = 16;
