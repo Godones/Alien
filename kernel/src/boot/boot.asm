60923bbba2255de14fd0d68cbeffe6f6ee6ef150
Godones <1925466036@qq.com>
2022-12-03 21:48:33 +0800

    feat: add multi-core boot support
    rustsbi 0.2.0-alpha.10 is ok

--- kernel/src/boot/boot.asm
+++ kernel/src/boot/boot.asm
@@ -1,35 +1,23 @@
     .section .text.entry
     .globl _start
 _start:
-    #clear bss
-	la 		t0, sbss
-	la		t1, ebss
-	bgeu	t0, t1, 2f
-1:
-	sd		zero, (t0)
-	addi	t0, t0, 8
-	bltu	t0, t1, 1b
-2:
-    # alloc stack for cpu
-    # a0 == hartid
-    add t0,a0,0
-    slli t0,t0,12
-    la sp, bootstack+0x1000
+    mv tp,a0
+    la sp,bootstack
+    addi t0,a0,1
+    slli t0,t0,16
     add sp,sp,t0
+    call rust_main
 
-    bnez a0,sleep
 
-    j rust_main
 
  sleep:
     wfi
     j sleep
 
     .section .bss.stack
-    .align 12   # page align
     .global bootstack
 bootstack:
-    .space 4096 * 8 #
+    .space 4096 * 16 * 4 #
     .global bootstacktop
 bootstacktop:
 
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,6 +6,7 @@
 
 #[macro_use]
 mod print;
+mod arch;
 mod config;
 mod driver;
 mod memory;
@@ -19,36 +20,83 @@ mod trap;
 extern crate log;
 extern crate alloc;
 
-use crate::config::{FRAME_SIZE, TIMER_FREQ};
-use crate::timer::read_timer;
+use crate::config::{CPU_NUM, FRAME_SIZE, TIMER_FREQ};
 use cfg_if::cfg_if;
 use core::arch::global_asm;
+use core::hint::spin_loop;
+use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
 global_asm!(include_str!("boot/boot.asm"));
+
+// 多核启动标志
+static STARTED: AtomicBool = AtomicBool::new(false);
+
+static CPUS: AtomicUsize = AtomicUsize::new(0);
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+    }
+}
 
 /// rust 入口函数
 ///
 /// 进行操作系统的初始化，
 #[no_mangle]
-pub extern "C" fn rust_main(_hart_id: usize, _device_tree_addr: usize) -> ! {
-    println!("{}", config::FLAG);
-    print::init_logger();
-    preprint::init_print(&print::PrePrint);
-    memory::init_frame_allocator();
-    memory::init_slab_system(FRAME_SIZE, 32);
-    cfg_if!(
+pub extern "C" fn rust_main(hart_id: usize, _device_tree_addr: usize) -> ! {
+    if hart_id == 0 {
+        clear_bss();
+        print::init_logger();
+        preprint::init_print(&print::PrePrint);
+        memory::init_frame_allocator();
+        memory::init_slab_system(FRAME_SIZE, 32);
+        println!("{}", config::FLAG);
+        cfg_if!(
         if #[cfg(feature = "test")] {
             memory::test_simple_bitmap();
             memory::frame_allocator_test();
             memory::test_heap();
             memory::test_page_allocator();
         }
-    );
-    memory::build_kernel_address_space();
-    memory::activate_paging_mode();
-    let time = read_timer();
-    println!("time: {}", time);
-    trap::init_trap_subsystem();
-    timer::set_next_trigger(TIMER_FREQ);
-    loop {}
+        );
+        memory::build_kernel_address_space();
+        memory::activate_paging_mode();
+        thread_local_init();
+        trap::init_trap_subsystem();
+        timer::set_next_trigger(TIMER_FREQ);
+        CPUS.fetch_add(1, Ordering::Release);
+        STARTED.store(true, Ordering::Relaxed);
+    } else {
+        while !STARTED.load(Ordering::Relaxed) {
+            spin_loop();
+        }
+        memory::activate_paging_mode();
+        thread_local_init();
+        trap::init_trap_subsystem();
+        timer::set_next_trigger(TIMER_FREQ);
+        CPUS.fetch_add(1, Ordering::Release);
+    }
+    wait_all_cpu_start(); //等待其它cpu核启动
+    loop {
+        spin_loop();
+    }
+}
+
+pub fn thread_local_init() {
+    // 允许内核读写用户态内存
+    // 取决于 CPU 的 RISC-V 规范版本就行处理
+    unsafe {
+        riscv::register::sstatus::set_sum();
+    }
+}
+
+fn wait_all_cpu_start() {
+    while CPUS.load(Ordering::Acquire) < CPU_NUM {
+        spin_loop()
+    }
 }
