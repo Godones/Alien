486f7b82bbfe56dae7281a13caf580a4814234da
Godones <1925466036@qq.com>
2023-03-30 20:45:53 +0800

    feat: add uart module

---
+++ modules/uart/examples/uart.rs
@@ -0,0 +1,35 @@
+use std::alloc::{alloc, Layout};
+use std::sync::Arc;
+use uart::Uart;
+
+enum ProcessState{
+    Waiting,
+    Readying,
+}
+struct Process{
+    state:ProcessState
+}
+
+fn test_uart(){
+    let addr = unsafe { alloc(Layout::from_size_align(8, 1).unwrap()) };
+    let addr = addr as usize;
+    let uart = Uart::<Arc<Process>>::new(addr);
+    let process = Arc::new(Process{state:ProcessState::Readying});
+
+    let wait = |x:&mut Vec<Arc<Process>>| {
+        let process = process.clone();
+        x.push(process);
+        println!("{}",x.len());
+    };
+
+    let schedule = ||{
+        println!("schedule ....");
+    };
+    for _i in 0..128{
+        uart.put(1u8,wait ,schedule);
+    }
+    // uart.put(1u8,wait ,schedule);
+}
+fn main(){
+    test_uart();
+}
