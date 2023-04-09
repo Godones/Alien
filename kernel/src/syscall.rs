7d5f3a7c66afb9934d47eff8c89a8791a2dad165
Godones <1925466036@qq.com>
2023-04-09 21:37:52 +0800

    fix: update dep

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -4,10 +4,15 @@ static SYSCALL_TABLE: Once<Table> = Once::new();
 pub fn register_all_syscall(){
 	let mut table = Table::new();
 	register_syscall!(table,
-	(1001, sys_create_global_bucket),
-	(1002, sys_execute_user_func),
-	(1003, sys_show_dbfs),
-	(1004, sys_execute_user_operate),
+	(210, shutdown),
+	(169, get_time_ms),
+	(1005, sleep),
+	(93, do_exit),
+	(124, do_suspend),
+	(172, get_pid),
+	(220, do_fork),
+	(221, do_exec),
+	(260, wait_pid),
 	(56, sys_openat),
 	(57, sys_close),
 	(61, sys_getdents),
@@ -42,15 +47,10 @@ pub fn register_all_syscall(){
 	(14, sys_removexattr),
 	(15, sys_lremovexattr),
 	(16, sys_fremovexattr),
-	(169, get_time_ms),
-	(1005, sleep),
-	(210, shutdown),
-	(93, do_exit),
-	(124, do_suspend),
-	(172, get_pid),
-	(220, do_fork),
-	(221, do_exec),
-	(260, wait_pid),
+	(1001, sys_create_global_bucket),
+	(1002, sys_execute_user_func),
+	(1003, sys_show_dbfs),
+	(1004, sys_execute_user_operate),
 
 	);
 	SYSCALL_TABLE.call_once(||table);
@@ -63,50 +63,50 @@ pub fn do_syscall(id:usize,args:&[usize])->isize{
 	    return res.unwrap();
 	}
 }
-use crate::fs::sys_truncate;
-use crate::fs::sys_symlinkat;
-use crate::fs::sys_fgetxattr;
-use crate::fs::sys_lremovexattr;
-use crate::fs::sys_flistxattr;
-use crate::fs::sys_execute_user_func;
-use crate::fs::sys_show_dbfs;
-use crate::fs::sys_list;
-use crate::fs::sys_getcwd;
-use crate::fs::sys_ftruncate;
-use crate::fs::sys_write;
-use crate::fs::sys_fstat;
-use crate::fs::sys_linkat;
-use crate::fs::sys_fstatfs;
-use crate::fs::sys_openat;
-use crate::fs::sys_statfs;
-use crate::fs::sys_close;
-use crate::fs::sys_renameat;
-use crate::fs::sys_execute_user_operate;
-use crate::fs::sys_lsetxattr;
-use crate::sbi::shutdown;
-use crate::task::do_exit;
 use crate::fs::sys_listxattr;
-use crate::timer::get_time_ms;
+use crate::fs::sys_llistxattr;
+use crate::fs::sys_flistxattr;
+use crate::fs::sys_mkdirat;
+use crate::fs::sys_show_dbfs;
+use crate::fs::sys_read;
+use crate::fs::sys_list;
+use crate::fs::sys_removexattr;
+use crate::task::do_exec;
+use crate::fs::sys_statfs;
+use crate::task::do_suspend;
+use crate::fs::sys_execute_user_func;
+use crate::sbi::shutdown;
+use crate::fs::sys_execute_user_operate;
+use crate::task::wait_pid;
+use crate::fs::sys_chdir;
+use crate::fs::sys_mkdir;
+use crate::fs::sys_readlinkat;
+use crate::fs::sys_lremovexattr;
+use crate::fs::sys_fremovexattr;
 use crate::fs::sys_unlinkat;
 use crate::fs::sys_getdents;
-use crate::task::do_fork;
-use crate::fs::sys_mkdir;
-use crate::fs::sys_read;
-use crate::fs::sys_setxattr;
-use crate::fs::sys_fremovexattr;
-use crate::fs::sys_readlinkat;
-use crate::fs::sys_fsetxattr;
-use crate::fs::sys_mkdirat;
-use crate::fs::sys_lseek;
-use crate::fs::sys_chdir;
-use crate::fs::sys_lgetxattr;
-use crate::fs::sys_removexattr;
-use crate::fs::sys_getxattr;
-use crate::timer::sleep;
-use crate::task::do_exec;
-use crate::task::get_pid;
-use crate::task::wait_pid;
-use crate::fs::sys_llistxattr;
-use crate::fs::sys_create_global_bucket;
+use crate::fs::sys_truncate;
+use crate::fs::sys_ftruncate;
+use crate::fs::sys_fstat;
 use crate::fs::sys_fstateat;
-use crate::task::do_suspend;
+use crate::fs::sys_create_global_bucket;
+use crate::fs::sys_lsetxattr;
+use crate::fs::sys_write;
+use crate::fs::sys_getcwd;
+use crate::timer::get_time_ms;
+use crate::fs::sys_getxattr;
+use crate::fs::sys_openat;
+use crate::fs::sys_fgetxattr;
+use crate::timer::sleep;
+use crate::task::get_pid;
+use crate::fs::sys_fsetxattr;
+use crate::fs::sys_lgetxattr;
+use crate::fs::sys_symlinkat;
+use crate::task::do_fork;
+use crate::fs::sys_close;
+use crate::fs::sys_linkat;
+use crate::fs::sys_lseek;
+use crate::fs::sys_setxattr;
+use crate::task::do_exit;
+use crate::fs::sys_renameat;
+use crate::fs::sys_fstatfs;
