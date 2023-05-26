28b7f164ea7e5cb7f6971bb324380dd28628f149
Godones <1925466036@qq.com>
2023-05-26 19:59:03 +0800

    fix: fix some errors about memory
    memory size is 256MB now

--- apps/filetest_simple/src/main.rs
+++ apps/filetest_simple/src/main.rs
@@ -5,36 +5,38 @@
 extern crate Mstd;
 extern crate alloc;
 
+use alloc::vec;
 use alloc::vec::Vec;
-use Mstd::fs::{close, open, read, write, OpenFlags, seek};
+
+use Mstd::fs::{close, open, OpenFlags, read, seek, write};
 
 #[no_mangle]
 pub fn main() -> i32 {
-    base_read_write_test_fs("/db/dbf1.txt\0");
-    base_read_write_test_fs("/fatf1.txt\0");
+    base_read_write_test_fs("/db/dbsimple.txt\0");
+    base_read_write_test_fs("/fatsimple.txt\0");
     0
 }
 
 
-fn base_read_write_test_fs(file:&str){
-    const DATA_SIZE: usize = 4096;
-    const STR:&[u8] = b"Hello, world!";
+fn base_read_write_test_fs(file: &str) {
+    const DATA_SIZE: usize = 1024 * 1024 * 2;
+    const STR: &[u8] = b"Hello, world!";
     println!("Test basic read/write on file {}", file);
-    let data = (0..4096).
-        map(|index|{STR[index % STR.len()]})
+    let data = (0..DATA_SIZE).
+        map(|index| { STR[index % STR.len()] })
         .collect::<Vec<u8>>();
     let fd = open(file, OpenFlags::O_CREAT | OpenFlags::O_WRONLY);
     assert!(fd > 0);
     let fd = fd as usize;
     write(fd, &data);
-    let mut  read_buf = [0u8; DATA_SIZE];
+    let mut read_buf = vec![0u8; DATA_SIZE];
     let read_len = read(fd, &mut read_buf) as usize;
     assert_eq!(read_len, 0);
     let r = seek(fd, 0, 0);
     assert_eq!(r, 0);
     let read_len = read(fd, &mut read_buf) as usize;
     assert_eq!(read_len, DATA_SIZE);
-    assert_eq!(data, read_buf.to_vec());
+    assert_eq!(data, read_buf);
     close(fd);
     println!("Test basic read/write on file {} passed", file);
 }
--- apps/randrw/src/main.rs
+++ apps/randrw/src/main.rs
@@ -2,6 +2,10 @@
 #![no_std]
 
 
+extern crate alloc;
+
+use alloc::vec;
+
 use rand::{RngCore, SeedableRng};
 use rand::rngs::SmallRng;
 
@@ -11,16 +15,16 @@ use Mstd::time::get_time_ms;
 
 const FILE_SIZE: usize = 1024 * 1024 * 16;
 //16MB
-const BLOCK_SIZE: usize = 4096;
+const BLOCK_SIZE: usize = 1024 * 1 * 1024;
 
-const ITER: usize = 1000_0;
+const ITER: usize = 1000;
 
 fn rand_read_write_test(path: &str) {
     let time = get_time_ms();
     let mut small_rng = SmallRng::seed_from_u64(time as u64);
     let fd = open(path, OpenFlags::O_RDWR | OpenFlags::O_CREAT);
     assert!(fd > 0);
-    let mut buf = [0u8; BLOCK_SIZE];
+    let mut buf = vec![0u8; BLOCK_SIZE];
     for _ in 0..FILE_SIZE / BLOCK_SIZE {
         let w = write(fd as usize, &buf);
         assert_eq!(w as usize, BLOCK_SIZE);
@@ -34,22 +38,22 @@ fn rand_read_write_test(path: &str) {
     let mut count = 0;
     for _ in 0..ITER {
         let offset = small_rng.next_u64();
-        let offset = offset % FILE_SIZE as u64;
-        let offset = offset as usize;
+        let offset = offset % (FILE_SIZE as u64 - BLOCK_SIZE as u64);
         let res = seek(fd as usize, offset as isize, 0);
         assert_ne!(res, -1);
         let read = read(fd as usize, &mut buf);
         assert_ne!(read, -1);
+        assert_eq!(read as usize, BLOCK_SIZE);
         count += read;
     }
     let end = get_time_ms();
     let time = end - start;
     let ops = ITER as f64 * 1000 as f64 / time as f64;
-    let throughput = count as f64 * 1000 as f64 / 1024 as f64 / time as f64;
+    let throughput = count as f64 * 1000 as f64 / 1024 as f64 / time as f64 / 1024 as f64;
     println!("Random read test:");
     println!("Elapsed time: {}ms", time);
-    println!("Read: {}KB", count / 1024);
-    println!("Throughput: {}KB/s", throughput);
+    println!("Read: {}MB", count / 1024 / 1024);
+    println!("Throughput: {}MB/s", throughput);
     println!("Operations: {}ops/s", ops);
 
     // rand write
@@ -57,22 +61,22 @@ fn rand_read_write_test(path: &str) {
     let mut count = 0;
     for _ in 0..ITER {
         let offset = small_rng.next_u64();
-        let offset = offset % FILE_SIZE as u64;
-        let offset = offset as usize;
+        let offset = offset % (FILE_SIZE as u64 - BLOCK_SIZE as u64);
         let res = seek(fd as usize, offset as isize, 0);
         assert_ne!(res, -1);
         let w = write(fd as usize, &buf);
         assert_ne!(w, -1);
         count += w;
     }
+    println!("write over");
     let end = get_time_ms();
     let time = end - start;
     let ops = ITER as f64 * 1000 as f64 / time as f64;
-    let throughput = count as f64 * 1000 as f64 / 1024 as f64 / time as f64;
+    let throughput = count as f64 * 1000 as f64 / 1024 as f64 / time as f64 / 1024 as f64;
     println!("Random write test:");
     println!("Elapsed time: {}ms", time);
-    println!("Write: {}KB", count / 1024);
-    println!("Throughput: {}KB/s", throughput);
+    println!("Write: {}MB", count / 1024 / 1024);
+    println!("Throughput: {}MB/s", throughput);
     println!("Operations: {}ops/s", ops);
     close(fd as usize);
 }
