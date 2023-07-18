fad905218ea199f398509c8f321d5f6d58c01e8a
BITcyman <815207911@qq.com>
2023-07-18 15:29:15 +0800

    feat: net driver

--- kernel/src/driver/mod.rs
+++ kernel/src/driver/mod.rs
@@ -3,6 +3,7 @@ pub use dtb::{init_dt, DEVICE_TABLE, PLIC};
 pub use input::sys_event_get;
 pub use mpci::pci_probe;
 
+
 mod block_device;
 mod dtb;
 mod hal;
@@ -12,6 +13,7 @@ pub mod gpu;
 pub mod input;
 pub mod rtc;
 pub mod uart;
+pub mod net;
 
 pub trait DeviceBase: Sync + Send {
     fn hand_irq(&self);
