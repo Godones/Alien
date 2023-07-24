ae561d1ff57567bd20562b149b57e051cc5ba4fd
Godones <1925466036@qq.com>
2023-07-24 23:34:15 +0800

    test

--- apps/init/src/main.rs
+++ apps/init/src/main.rs
@@ -30,20 +30,38 @@ fn main() -> isize {
     0
 }
 
+const ENV: &[*const u8] = &[
+    "SHELL=/bash\0".as_ptr(),
+    "PWD=/\0".as_ptr(),
+    "LOGNAME=root\0".as_ptr(),
+    "MOTD_SHOWN=pam\0".as_ptr(),
+    "HOME=/root\0".as_ptr(),
+    "LANG=C.UTF-8\0".as_ptr(),
+    "TERM=vt220\0".as_ptr(),
+    "USER=root\0".as_ptr(),
+    "SHLVL=0\0".as_ptr(),
+    "OLDPWD=/root\0".as_ptr(),
+    "PS1=\x1b[1m\x1b[32mAlien\x1b[0m:\x1b[1m\x1b[34m\\w\x1b[0m\\$ \0".as_ptr(),
+    "_=/bin/bash\0".as_ptr(),
+    "PATH=/:/bin\0".as_ptr(),
+    "LD_LIBRARY_PATH=/\0".as_ptr(),
+    core::ptr::null(),
+];
+
 fn run_test() {
     let commands = [
         "./time-test\0",
         "./busybox_testcode.sh\0",
-        "./iozone_testcode.sh\0",
         "./lua_testcode.sh\0",
         "./libctest_testcode.sh\0",
         "./cyclictest_testcode.sh\0",
+        "./iozone_testcode.sh\0",
     ];
     commands.into_iter().for_each(|app| {
         let args = [app.as_ptr()];
         let pid = fork();
         if pid == 0 {
-            exec(app, &args, &[0 as *const u8]);
+            exec(app, &args, ENV);
             exit(0);
         } else {
             m_yield();
