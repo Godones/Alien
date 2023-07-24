f1d2b7083707f1e38fe395226f8e3a6ba5bd55ca
Godones <1925466036@qq.com>
2023-07-24 12:53:17 +0800

    test: try upload test with iozone

--- kernel/src/config.rs
+++ kernel/src/config.rs
@@ -19,7 +19,7 @@ pub const STACK_SIZE: usize = 1024 * 64;
 pub const STACK_SIZE_BITS: usize = 16;
 
 pub const TIMER_FREQ: usize = CLOCK_FREQ;
-pub const CPU_NUM: usize = 1;
+pub const CPU_NUM: usize = 4;
 
 pub const MMIO: &[(usize, usize)] = &[
     (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
@@ -38,10 +38,13 @@ pub const PROCESS_HEAP_MAX: usize = u32::MAX as usize + 1;
 pub const TRAMPOLINE: usize = usize::MAX - 2 * FRAME_SIZE + 1;
 pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - FRAME_SIZE;
 
-// 栈大小16k
-pub const USER_KERNEL_STACK_SIZE: usize = 0x1000 * 4;
-// 64KB
+// app内核栈大小
+pub const USER_KERNEL_STACK_SIZE: usize = 0x2000;
+// 用户栈大小
 pub const USER_STACK_SIZE: usize = 0x50000;
+
+#[cfg(not(feature = "slab"))]
+pub const KERNEL_HEAP_SIZE: usize = 0x10_00000;// 16MB
 
 // 进程数量/线程数量/描述符表大小限制
 pub const MAX_PROCESS_NUM: usize = 1024;
