91aded6e4d0eaff6d7ffe73b71b04d46dbb0f7a9
Godones <1925466036@qq.com>
2023-08-20 17:31:44 +0800

    nothing

--- kernel/src/config.rs
+++ kernel/src/config.rs
@@ -36,7 +36,7 @@ pub const STACK_SIZE_BITS: usize = 16;
 /// equal to CLOCK_FREQ
 pub const TIMER_FREQ: usize = CLOCK_FREQ;
 /// 可配置的启动cpu数量
-pub const CPU_NUM: usize = 1;
+pub const CPU_NUM: usize = 2;
 
 ///qemu的设备地址空间
 #[cfg(feature = "qemu")]
@@ -197,6 +197,5 @@ root:x:0:0:root:/root:/bin/bash
 /// 10：串口中断
 /// 11：块设备中断
 pub const INTERRUPT_RECORD: &str = r"
-1: 1
-4: 1
+1: 0
 ";
