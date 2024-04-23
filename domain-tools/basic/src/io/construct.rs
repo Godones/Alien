158d8a4cb89d8576c2dcf8d0ded56af219ea0fd2
Godones <1925466036@qq.com>
2024-04-23 09:18:08 +0800

    update doc

--- domain-tools/basic/src/io/construct.rs
+++
@@ -1,7 +0,0 @@
-use memory_addr::VirtAddr;
-
-/// # Safety
-/// caller must ensure that memory area is available during return value's lifetime
-pub fn construct_ref_mut<'a, T>(va: VirtAddr) -> &'a mut T {
-    unsafe { &mut *((va.as_usize()) as *mut T) }
-}
