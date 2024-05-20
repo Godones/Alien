76590b81eece507ec9e048b1915919dd68f0b638
Godones <1925466036@qq.com>
2024-05-20 20:35:07 +0800

    fix: update Makefile

--- Makefile
+++ Makefile
@@ -90,6 +90,8 @@ mount:
 
 
 domains:
+	if [ ! -d "build/disk" ]; then mkdir -p build/disk; fi
+	if [ ! -d "build/init" ]; then mkdir -p build/init; fi
 	cd domains && cargo domain build-all -l "$(LOG)"
 	cp domains/build/disk/* build/disk/ -r
 	cp domains/build/init/* build/init/ -r
--- kernel/build.rs
+++ kernel/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let outdir = env::var("OUT_DIR").unwrap();
     let link_script = Path::new(&outdir).join("link.lds");
     let mut script = File::create(&link_script).unwrap();
-    let ld_path = Path::new("../build/link.ld");
+    let ld_path = Path::new("../tools/link.ld");
     let ld = fs::read_to_string(ld_path).unwrap();
     script.write_all(ld.as_bytes()).unwrap();
     println!("cargo:rustc-link-arg=-T{}", &link_script.display());
